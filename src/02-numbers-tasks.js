57f25b4cbea4ce3c3cbec5c4922d832ec66adb7b
miarur <deadheartedfo@yahoo.com>
2022-07-23 23:43:20 +0300

    feat: done 30 tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -230,8 +230,15 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const val = parseInt(value, 10);
+  if (Number.isNaN(val)) {
+    return def;
+  }
+  if (typeof val === 'number') {
+    return val;
+  }
+  return def;
 }
 
 module.exports = {
